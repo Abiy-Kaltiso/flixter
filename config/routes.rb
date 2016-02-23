d2c1fe63d9c9097c50c480db3a00c75af4417494
Abiy-Kaltiso <abiyk7@gmail.com>
2016-02-23 23:37:43 +0000

    added additional validations to courses

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Thinkly::Application.routes.draw do
     resources :courses, only: [:new, :create, :show]
   end 
 
+  resources :courses, only: [:index, :show]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
