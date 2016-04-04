639ce2338173a9e7088e903db63b3078f5d47eca
Abiy-Kaltiso <abiyk7@gmail.com>
2016-04-04 19:37:20 +0000

    created page to add lesson sections

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Thinkly::Application.routes.draw do
 
   devise_for :users
   root 'static_pages#index'
-  namespace :instructor do 
-    resources :courses, only: [:new, :create, :show]
-  end 
-
   resources :courses, only: [:index, :show]
 
+  namespace :instructor do 
+    resources :courses, only: [:new, :create, :show] do 
+      resources :sections, only: [:new, :create]
+    end 
+  end 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
